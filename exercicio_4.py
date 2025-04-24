d41d94476784bf57a594892aaa95f2de69a9d73a
DaniFins03 <danifinsbiomed@gmail.com>
2025-04-24 17:00:41 -0300

    Update exercicio_4.py

--- exercicio_4.py
+++ exercicio_4.py
@@ -35,10 +35,6 @@ Dica: Utilize mais de um for para resolver o exercício, um para a lista de orga
 
 O cálculo de média já foi feito em sala e pode ser usado de exemplo.
 """
-
-# Lista
-lista_de_organismos = [[50, 50, 50], [125, 99, 12], [19, 91, 42], [40, 189, 0], [1, 0, 0], [100, 100, 70], [99, 12, 12]]
-
 lista_de_organismos = [[50, 50, 50], [125, 99, 12], [19, 91, 42], [40, 189, 0], [1, 0, 0], [100, 100, 70], [99, 12, 12]]
 maior_media = 0 
 maior_posicao = 0 
