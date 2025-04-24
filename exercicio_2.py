ea191f7b8a171fd54e67ed7cf8d03a2f589154aa
DaniFins03 <danifinsbiomed@gmail.com>
2025-04-24 17:06:37 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -18,6 +18,7 @@ Digite a sua lista (separando os números por vírgula): 1, 2, 3, 4, 5
 Resposta:
 Os números ímpares são [1, 3, 5]
 """
+
 # Código para pegar a lista
 lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
 impares = []
